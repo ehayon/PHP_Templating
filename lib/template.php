2ea50d40c0b17d6e449e63b0dec2a73f49128116
Ethan Hayon <ehayon@gmail.com>
2012-11-27 15:26:30 -0500

    templating with layout

--- lib/template.php
+++ lib/template.php
@@ -21,11 +21,26 @@ class Template
     public function set($attr, $value) {
         $this->attributes[$attr] = $value;
     }
-
     public function render() {
+		// check to see if there is a layout defined
+		// look for %%layout:<path>%%
         foreach($this->attributes as $attr => $value) {
             $this->contents = str_replace('['.$attr.']', $value, $this->contents);
         }
+
+		$matches = array();
+		preg_match('/\%\%layout:(.+)\%\%/', $this->contents, $matches);
+		if(count($matches) > 1) {
+			$this->contents = preg_replace('/\%\%layout:(.+)\%\%/', '', $this->contents);
+
+			preg_match_all('/{(.+)}(.*){\/\1}/s', $this->contents, $m);
+			$layout = new Template($matches[1]);
+			foreach($m[0] as $key) {
+				preg_match('/{(.+)}(.*){\/\1}/s', $key, $tmp);
+				$layout->set($tmp[1], $tmp[2]);
+			}
+			return $layout->render();	
+		}
         return $this->contents;
     }
 
